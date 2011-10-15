ffaf0d10dd16341ffe3e8cb9a1d90bd20996bf8c
aarongough <aaron@aarongough.com>
2011-10-15 18:32:22 -0400

    Added implementation for Vertex class.

--- lib/triangular/vertex.rb
+++ lib/triangular/vertex.rb
@@ -1,5 +1,24 @@
 module Triangular
   class Vertex
     
+    attr_accessor :x, :y, :z
+    
+    def initialize(x, y, z)
+      @x = x
+      @y = y
+      @z = z      
+    end
+    
+    def self.parse(string)
+      string.strip!
+      match_data = string.match(self.pattern)
+      
+      self.new(match_data[1].to_f, match_data[2].to_f, match_data[3].to_f)
+    end
+    
+    def self.pattern
+      /vertex\s+(-?\d+.\d+)\s(-?\d+.\d+)\s(-?\d+.\d+)/
+    end
+    
   end
 end
