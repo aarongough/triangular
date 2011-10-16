ea2b90647db89230b809edc393abd5605a54dba2
aarongough <aaron@aarongough.com>
2011-10-15 21:03:04 -0400

    Added implementation for Solid.

--- lib/triangular.rb
+++ lib/triangular.rb
@@ -2,6 +2,7 @@ require 'triangular/point'
 require 'triangular/vertex'
 require 'triangular/vector'
 require 'triangular/facet'
+require 'triangular/solid'
 
 module Triangular
   # Your code goes here...
--- lib/triangular/solid.rb
+++ lib/triangular/solid.rb
@@ -1,3 +1,23 @@
 module Triangular
-  # Your code goes here...
+  class Solid
+    
+    attr_accessor :name, :facets
+    
+    def initialize(name, *args)
+      @name = name
+      @facets = args
+    end
+    
+    def self.parse(string)
+      match_data = string.match(/\s* solid\s+ (?<name> [a-zA-Z0-9\-\_\.]+)?/x)
+      
+      solid = self.new(match_data[:name])
+      
+      string.gsub(Facet.pattern) do |facet|
+        solid.facets << Facet.parse(facet)
+      end
+      
+      solid
+    end
+  end
 end
