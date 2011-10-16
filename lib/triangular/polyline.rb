958776e3db5ac6e6a595d95518244c3d069b3510
aarongough <aaron@aarongough.com>
2011-10-16 18:13:19 -0400

    Added support for exporting Polylines as SVG images.

--- lib/triangular/polyline.rb
+++ lib/triangular/polyline.rb
@@ -7,5 +7,18 @@ module Triangular
       @lines = lines
     end
     
+    def to_svg
+      output  = '<?xml version="1.0" standalone="no"?>' + "\n"
+      output << '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' + "\n"
+      output << '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">' + "\n"
+      output << '  <g transform="translate(200,200)">' + "\n"
+      
+      @lines.each do |line|
+        output << "    " + line.to_svg_path + "\n"
+      end
+      
+      output << '  </g>' + "\n"
+      output << '</svg>'
+    end
   end
 end
