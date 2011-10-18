97e41c58ba4116a78af961c23758e2c629374774
aarongough <aaron@aarongough.com>
2011-10-17 21:53:23 -0400

    Removed superflous tests in line_spec.

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -37,115 +37,33 @@ describe Line do
   
   describe "#intersection_at_z" do
     context "for a line that intersects the target Z plane" do
-      context "and is located at the origin" do
+      context "and spans both positive and negative space" do
         context "with a positive Z vector" do
           before do
-            @line = Line.new(Vertex.new(0.0, 0.0, 0.0), Vertex.new(0.0, 0.0, 6.0))
-          end
-          
-          it "should return a a Point" do
-            @line.intersection_at_z(3.0).should be_a Point
+            @line = Line.new(Vertex.new(-1.0, 1.0, 1.0), Vertex.new(1.0, 1.0, -1.0))
           end
           
           it "should return a Point representing the intersection" do
-            @line.intersection_at_z(3.0).x.should == 0.0
-            @line.intersection_at_z(3.0).y.should == 0.0
-            @line.intersection_at_z(3.0).z.should == 3.0
+            @line.intersection_at_z(0).x.should == 0.0
+            @line.intersection_at_z(0).y.should == 1.0
+            @line.intersection_at_z(0).z.should == 0.0
           end
         end
         
         context "with a negative Z vector" do
           before do
-            @line = Line.new(Vertex.new(0.0, 0.0, 6.0), Vertex.new(0.0, 0.0, 0.0))
+            @line = Line.new(Vertex.new(1.0, 1.0, 1.0), Vertex.new(-1.0, -1.0, -1.0))
           end
           
           it "should return a Point representing the intersection" do
-            @line.intersection_at_z(3.0).x.should == 0.0
-            @line.intersection_at_z(3.0).y.should == 0.0
-            @line.intersection_at_z(3.0).z.should == 3.0
-          end
-        end
-      end
-      
-      context "and is located away from the origin" do
-        context "with a positive Z vector" do
-          before do
-            @line = Line.new(Vertex.new(7.0, 7.0, 1.0), Vertex.new(7.0, 7.0, 7.0))
-          end
-          
-          it "should return a Point representing the intersection" do
-            @line.intersection_at_z(4.0).x.should == 7.0
-            @line.intersection_at_z(4.0).y.should == 7.0
-            @line.intersection_at_z(4.0).z.should == 4.0
-          end
-        end
-        
-        context "with a negative Z vector" do
-          before do
-            @line = Line.new(Vertex.new(7.0, 7.0, 7.0), Vertex.new(1.0, 1.0, 1.0))
-          end
-          
-          it "should return a Point representing the intersection" do
-            @line.intersection_at_z(4.0).x.should == 4.0
-            @line.intersection_at_z(4.0).y.should == 4.0
-            @line.intersection_at_z(4.0).z.should == 4.0
+            @line.intersection_at_z(0).x.should == 0
+            @line.intersection_at_z(0).y.should == 0
+            @line.intersection_at_z(0).z.should == 0
           end
         end
       end
     end
-    
-    context "and is located in negative space" do
-      context "with a negative Z vector" do
-        before do
-          @line = Line.new(Vertex.new(-1.0, -1.0, -1.0), Vertex.new(-7.0, -7.0, -7.0))
-        end
-        
-        it "should return a Point representing the intersection" do
-          @line.intersection_at_z(-4.0).x.should == -4.0
-          @line.intersection_at_z(-4.0).y.should == -4.0
-          @line.intersection_at_z(-4.0).z.should == -4.0
-        end
-      end
       
-      context "with a positive Z vector" do
-        before do
-          @line = Line.new(Vertex.new(-7.0, -7.0, -7.0), Vertex.new(-1.0, -1.0, -1.0))
-        end
-        
-        it "should return a Point representing the intersection" do
-          @line.intersection_at_z(-4.0).x.should == -4.0
-          @line.intersection_at_z(-4.0).y.should == -4.0
-          @line.intersection_at_z(-4.0).z.should == -4.0
-        end
-      end
-    end
-    
-    context "and spans both positive and negative space" do
-      context "with a positive Z vector" do
-        before do
-          @line = Line.new(Vertex.new(-1.0, 1.0, 1.0), Vertex.new(1.0, 1.0, -1.0))
-        end
-        
-        it "should return a Point representing the intersection" do
-          @line.intersection_at_z(0).x.should == 0.0
-          @line.intersection_at_z(0).y.should == 1.0
-          @line.intersection_at_z(0).z.should == 0.0
-        end
-      end
-      
-      context "with a negative Z vector" do
-        before do
-          @line = Line.new(Vertex.new(1.0, 1.0, 1.0), Vertex.new(-1.0, -1.0, -1.0))
-        end
-        
-        it "should return a Point representing the intersection" do
-          @line.intersection_at_z(0).x.should == 0
-          @line.intersection_at_z(0).y.should == 0
-          @line.intersection_at_z(0).z.should == 0
-        end
-      end
-    end
-    
     context "for a line that lies on the target Z plane" do
       before do
         @line = Line.new(Vertex.new(0.0, 0.0, 3.0), Vertex.new(0.0, 6.0, 3.0))
