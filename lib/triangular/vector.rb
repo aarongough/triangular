f644a964904a3f742ed568ff1a4e8fc6611601d6
aarongough <aaron@aarongough.com>
2011-10-23 19:16:26 -0400

    Added tests and implementation for Vector#angle_to(other_vector)

--- lib/triangular/vector.rb
+++ lib/triangular/vector.rb
@@ -1,4 +1,11 @@
 module Triangular
-  class Vector < Point    
+  class Vector < Point
+    
+    def angle_to(other_vector)
+      dot_product = @x*other_vector.x + @y*other_vector.y + @z*other_vector.z
+      radians = Math.acos(dot_product);
+      radians * (180.0 / Math::PI)
+    end
+    
   end
 end
