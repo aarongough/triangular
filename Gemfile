e463e9ca9cd48191d533ea7ff3c9aba7eee706fc
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2014-07-09 23:50:34 +0800

    apply travis

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
 
+group :test do
+  gem 'rake'
+end
+
 # Specify your gem's dependencies in triangular.gemspec
 gemspec
--- features/step_definitions/cli.rb
+++ features/step_definitions/cli.rb
@@ -1,7 +1,3 @@
-Given(/^fixture "([^"]*?)" is copied to testing directory$/) do |filename|
-  FileUtils.cp "spec/fixtures/#{filename}", "tmp/aruba/"
-end
-
 Then(/^I should see the right inc content$/) do
   expect(all_output).to eq(<<-EOT)
 # declare m_0 = mesh {
